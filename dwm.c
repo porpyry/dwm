df531e6620de8cafc61bcd33d9970f46518b5511
porpyry <porpyry@gmail.com>
2022-05-26 00:38:14 +0900

    float

--- dwm.c
+++ dwm.c
@@ -1609,20 +1609,24 @@ movestack(const Arg *arg) {
 void
 movecenter(const Arg *arg)
 {
-	selmon->sel->x = selmon->sel->mon->mx + (selmon->sel->mon->mw - WIDTH(selmon->sel)) / 2;
-	selmon->sel->y = selmon->sel->mon->my + (selmon->sel->mon->mh - HEIGHT(selmon->sel)) / 2;
-	arrange(selmon);
+	if (selmon->sel->isfloating) {
+		selmon->sel->x = selmon->sel->mon->mx + (selmon->sel->mon->mw - WIDTH(selmon->sel)) / 2;
+		selmon->sel->y = selmon->sel->mon->my + (selmon->sel->mon->mh - HEIGHT(selmon->sel)) / 2;
+		arrange(selmon);
+	}
 }
 
 void
 resizecenter(const Arg *arg)
 {
-	selmon->sel->w = selmon->ww * 2 / 3;
-	selmon->sel->h = selmon->wh * 2 / 3;
-	selmon->sel->x = selmon->sel->mon->mx + (selmon->sel->mon->mw - WIDTH(selmon->sel)) / 2;
-	selmon->sel->y = selmon->sel->mon->my + (selmon->sel->mon->mh - HEIGHT(selmon->sel)) / 2;
-	resizeclient(selmon->sel, selmon->sel->x, selmon->sel->y, selmon->sel->w, selmon->sel->h);
-	arrange(selmon);
+	if (selmon->sel->isfloating) {
+		selmon->sel->w = selmon->ww / 2;
+		selmon->sel->h = selmon->wh / 2;
+		selmon->sel->x = selmon->sel->mon->mx + (selmon->sel->mon->mw - WIDTH(selmon->sel)) / 2;
+		selmon->sel->y = selmon->sel->mon->my + (selmon->sel->mon->mh - HEIGHT(selmon->sel)) / 2;
+		resizeclient(selmon->sel, selmon->sel->x, selmon->sel->y, selmon->sel->w, selmon->sel->h);
+		arrange(selmon);
+	}
 }
 
 Client *
@@ -2283,10 +2287,6 @@ togglefloating(const Arg *arg)
 		return;
 	selmon->sel->isfloating = !selmon->sel->isfloating || selmon->sel->isfixed;
 	if (selmon->sel->isfloating) {
-		selmon->sel->w = selmon->ww * 2 / 3;
-		selmon->sel->h = selmon->wh * 2 / 3;
-		selmon->sel->x = selmon->sel->mon->mx + (selmon->sel->mon->mw - WIDTH(selmon->sel)) / 2;
-		selmon->sel->y = selmon->sel->mon->my + (selmon->sel->mon->mh - HEIGHT(selmon->sel)) / 2;
 		resizeclient(selmon->sel, selmon->sel->x, selmon->sel->y, selmon->sel->w, selmon->sel->h);
 	}
 	arrange(selmon);
